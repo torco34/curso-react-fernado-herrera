e93bb53fbd6b362c6c99667fec71944e8c437030
torcoroma <torcoromaarias@gmail.com>
2023-08-28 09:58:09 -0500

    nuevo ejercicio

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import { useState } from "react";
 
 import "./App.css";
 import { Index } from "./component/repasoDeJs/Index";
-
+import { Ejercicio } from "./component/ejercicio/Ejercicio";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
       {/* <Index /> */}
+      <div className="container">
+        <Ejercicio
+          title="este es un prosp y suma tambien es una prosp"
+          suma={7 + 3}
+        />
+      </div>
     </>
   );
 }
