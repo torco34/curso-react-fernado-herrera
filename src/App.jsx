2c9a1fdc1f4973c8dd8c67d5c598d483d1b1e369
torcoroma <torcoromaarias@gmail.com>
2023-09-07 17:36:33 -0500

    nuevo proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,14 @@ import { useState } from "react";
 import "./App.css";
 import { Index } from "./component/repasoDeJs/Index";
 import { Ejercicio } from "./component/ejercicio/Ejercicio";
+import PegesProyecto from "./component/proyecto-01/PegesProyecto";
 function App() {
-  const [count, setCount] = useState(0);
+
 
   return (
     <>
-      {/* <Index /> */}
       <div className="container">
-        <Ejercicio
-          title="este es un prosp y suma tambien es una prosp"
-          suma={7 + 3}
-        />
+        <PegesProyecto />
       </div>
     </>
   );
